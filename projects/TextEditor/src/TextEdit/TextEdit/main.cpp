1ceedb14b3aee4c868de5dc009624b2f32ed820f
rja45 <54596406+rja45@users.noreply.github.com>
2019-09-04 12:03:30 -0700

    I changed the colors of the console window, added a header/welcome message, created an escape button (ESC) to exit the text editor, and I've indicated which keys can be used for saving, loading, and escaping.

--- projects/TextEditor/src/TextEdit/TextEdit/main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/main.cpp
@@ -3,19 +3,33 @@
 #include "curses.h"
 #include "panel.h"
 #include "curspriv.h"
+#include <iostream>
+#include <fstream>
 
-int main(void) 
+using namespace std;
+
+int main(void)
 {
 	WINDOW* main_window = nullptr;
 	int num_cols = 0;
 	int num_rows = 0;
 
 	//SETUP
+	
+
 	//initialize our window
 	main_window = initscr();
 
+	//start colors
+	start_color();
+
+	// Blue text on a white screen
+	init_pair(1, COLOR_BLUE, COLOR_WHITE);
+	bkgd(COLOR_PAIR(1)); 
+
+
 	//resize our window
-	resize_term(5000, 5000);
+	resize_term(0, 0);
 	getmaxyx(main_window, num_rows, num_cols);
 	resize_term(num_rows - 1, num_cols - 1);
 	getmaxyx(main_window, num_rows, num_cols);
@@ -25,36 +39,56 @@ int main(void)
 
 	//turn on keypad input
 	keypad(main_window, TRUE);
-	curs_set(FALSE);
+	curs_set(2);
 
-	//add character to the screen
-	mvaddch(10, 10, 'X');
+	//add Welcome String to the screen
+	mvaddstr(2, 2, "Welcome to the Airth Text Editor. Press ESC to exit.");
+
 
 	//MAIN PROGRAM LOGIC GOES HERE
 
-	for (int i = 0; i < num_cols; i++)
-	{
-		//top row
-		mvaddch(0, i, ACS_BLOCK);
+		//This is where the input characters get handled
 
-		//bottom row
-		mvaddch(num_rows - 1, i, ACS_BLOCK);
-	}
+	
+
+	for (int i = 0; i < num_cols; i++)
+		{
+			//top row
+			mvaddch(0, i, ACS_BLOCK);
+
+
+			//bottom row
+		
+			mvaddch(num_rows - 1, i, ACS_BLOCK);
+
+		}
 
 	for (int i = 0; i < num_rows; i++)
-	{
-		//left column
-		mvaddch(i, 0, ACS_BLOCK);
+		{
+			//left column
+			mvaddch(i, 0, ACS_BLOCK);
 
-		//right column
-		mvaddch(i, num_cols -1, ACS_BLOCK);
-	}
+			//right column
+			mvaddch(i, num_cols - 1, ACS_BLOCK);
+		}
+	mvaddstr(num_rows - 2, 3, "ESC = Quit Program | CTRL+S = Save | CTRL+L = Load");
 
 
-	//END OF PROGRAM LOGIC GOES HERE
+	wmove(main_window, 3, 3);
+
+		
+	
+int input = getch();
+
+while (input != 27) {
+
+
+	waddch(main_window, input);
+
 
 	//pause for user input
-	char input = getch();
+	input = getch();
+}
 	
 	//end curses mode
 	endwin();
