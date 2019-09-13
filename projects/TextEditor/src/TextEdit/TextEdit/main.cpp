34ee83f137828ee67b8afec95c84081c2bd42dc1
rja45 <54596406+rja45@users.noreply.github.com>
2019-09-12 19:47:31 -0700

    I refactored my code. I am now implementing a window within the stdscr for text to be input/output. I have also cleaned up a lot of the commented-out and useless code.

--- projects/TextEditor/src/TextEdit/TextEdit/main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/main.cpp
@@ -1,16 +1,24 @@
+//Define global variables
 #define PDC_DLL_BUILD 1
 #define PDC_WIDE 1
 #define WINCOLORS 1
 #define PROMPTCOLORS 2
+#define TERMCOLORS 3
+#define TERMTEXT 4
 #define ESCAPE 27
-#define WINDOWTEXT 3
+#define SAVE ctrl('s')
+#define LOAD ctrl('l')
+#define NEW_FILE ctrl('n')
+#define ctrl(x)           ((x) & 0x1f)
 
 
+//Header Files
 #include "curses.h"
 #include "panel.h"
 #include "curspriv.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -19,273 +27,277 @@ using namespace std;
 bool confirm(void);
 
 
+void draw_centered(WINDOW* win, int max_y, int max_x, string text);
+
 
 //******************************            Main Function                  *******************************
-int main(void){
+int main(void) {
 
-//////////////////////////////////          This is where the window is built      ////////////////////////
+	//////////////////////////////////          This is where the window is built      ////////////////////////
 
-	WINDOW* main_window = nullptr;
+		//create variables
+	int term_cols = 0;
+	int term_rows = 0;
+	int curs_x = 0;
+	int curs_y = 0;
+	vector <string> inputbuffer;
 
-	int num_cols = 0;
-	int num_rows = 0;
-	int cursx = 0;
-	int cursy = 0;
+	//initialize the terminal
+	initscr();
 
-	
+	//Resize for initial size
+	resize_term(30, 120);
+	getmaxyx(stdscr, term_rows, term_cols);
 
-	//initialize our window
-	resize_term(2000, 2000);
-	main_window=initscr();	
-		//initscr();
-		
-cbreak();
-keypad(main_window, TRUE);
-//raw();
+
+	//initialize the main window
+	WINDOW* main_window = newwin((term_rows - 4), (term_cols - 4), 2, 2);
+
+	//Set input
+	nl();
+
+	//cbreak();	
+
+	keypad(stdscr, true);
+
+	//keypad(main_window, true);
+
+	//raw();
+
+	//Set Scrolling
+
+	scrollok(main_window, true);
+
+	wsetscrreg(main_window, term_rows - 4, term_cols - 4);
+
+	//Set cursor visibility
+	curs_set(2);
+
+	//turn off keyboard echo
+	noecho();
+
+
+	if (can_change_color) {
+
+		//start colors
+		start_color();
+
+		// Black text on a white screen for regular Window Colors
+		init_pair(WINCOLORS, COLOR_BLACK, COLOR_WHITE);
+
+		// White text on a red screen for Prompt Colors
+		init_pair(PROMPTCOLORS, COLOR_WHITE, COLOR_RED);
+
+		// Blue border and white inserior for the Terminal Colors
+		init_pair(TERMCOLORS, COLOR_BLUE, COLOR_WHITE);
+
+		// White text to contrast with Blue Terminal border text.
+		init_pair(TERMTEXT, COLOR_WHITE, COLOR_BLUE);
+
+		// Initialize the background with WINCOLORS
+		wbkgd(main_window, COLOR_PAIR(WINCOLORS));
+
+		//Initialize the Terminal Colors
+		bkgd(COLOR_PAIR(TERMCOLORS));
+	}
+	else {
+		use_default_colors();
+	}
+
+	//creates the BLUE border around the perimeter of the terminal
+
+
+	border(ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK,
+		ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK);
+
+
+
+
+
+
+
+
+	//add Welcome String to the screen
+	attron(COLOR_PAIR(TERMTEXT)); //CHANGES COLOR FOR PROMPT
+
+	mvaddstr(0, 3, "Welcome to the Airth Text Editor!");
+
+	//attroff( COLOR_PAIR(TERMCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
+
+	//Footer message
+	//attron(COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
+
+	mvaddstr(term_rows - 1, 3, "ESC - Quit | CTRL+S -Save File | CTRL+L -Load File | CTRL+N -New File");
+
+	attroff(COLOR_PAIR(TERMTEXT)); //CHANGES COLOR BACK TO WINCOLORS
+
+	refresh();
+	//wrefresh(main_window);
+
+
+//sets cursor initial position
+//wmove(main_window, 3, 3);
 //wrefresh(main_window);
-//immedok(main_window, true);
 
-
-//turn off keyboard echo
-		noecho();
-
-	
-		
-		//curs_set(2);
-
-	//start colors
-	start_color();
-	init_pair(WINCOLORS, COLOR_BLUE, COLOR_WHITE);// Blue text on a white screen for regular Window Colors
-
-	init_pair(PROMPTCOLORS, COLOR_WHITE, COLOR_RED);// White text on a red screen for Prompt Colors
-
-	init_pair(WINDOWTEXT, COLOR_WHITE, COLOR_BLUE); //White text on a blue background for Window Text
-
-	bkgd(COLOR_PAIR(WINCOLORS));// Initialize the background with WINCOLORS
-
-
-		//resize our window
-		
-		getmaxyx(main_window, num_rows, num_cols);
-		refresh();
-		//resize_term(num_rows - 1, num_cols - 1);
-		//getmaxyx(main_window, num_rows, num_cols);
-		
-
-		//creates the border around the perimeter of the window
-		
-		wborder(main_window, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK,
-			ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK);
-		
-		
-		
-
-		//add Welcome String to the screen
-		wattron(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
-
-		mvaddstr(0, 3, "Welcome to the Airth Text Editor!");
-
-		wattroff(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS
-
-		//Footer message
-		wattron(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
-
-		mvaddstr(num_rows - 1, 3, "ESC - Quit | F1 - Save File | F2 - Load File | F3 - New File");
-
-		wattroff(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS
-
-	
-
-
-	//sets cursor initial position
-	wmove(main_window, 1, 3);
-
-	///////////////////////         This is where the input characters get handled           /////////////////
-
+///////////////////////         This is where the input characters get handled           /////////////////
+//set whether user wants to quit
 	bool quit = false;
-	int input = wgetch(main_window);
+
+	//get input from keyboard
+
+	char input = wgetch(main_window);
 
 	while (!quit) {
 
 		switch (input) {
 		case ESCAPE:
 			//changes color for prompt
-			wattron(main_window, COLOR_PAIR(PROMPTCOLORS));
-
+			attron(COLOR_PAIR(PROMPTCOLORS));
 			//exit prompt print
-			mvaddstr(num_rows - 2, num_cols - 45, "Are you sure you want to quit?");
+			mvaddstr(term_rows - 1, term_cols - 40, "Are you sure you want to quit?");
 
-			//changes color to wincolors
-			wattroff(main_window, COLOR_PAIR(PROMPTCOLORS));
 			quit = confirm();
 
-			wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
+			attron(COLOR_PAIR(TERMCOLORS));
 
-			mvaddstr(num_rows - 2, num_cols - 45, "Would you like to save your file? Y or N");
+			border(ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK,
+				ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK);
 
-			wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
+			attroff(COLOR_PAIR(TERMCOLORS));
+
+			mvaddstr(term_rows - 1, term_cols - 45, "Would you like to save your file? Y or N");
+
+			attroff(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
+
+			attron(COLOR_PAIR(TERMCOLORS));
+
+			border(ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK,
+				ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK);
+
+			attroff(COLOR_PAIR(TERMCOLORS));
 
 			if (confirm()) {
 				clear();
-				wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
-				mvaddstr(((num_rows / 2) - 2), ((num_cols / 2) - 12), "Your file has been saved!!");
-				wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
+				attron(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
+				mvaddstr(((term_rows / 2) - 2), ((term_cols / 2) - 12), "Your file has been saved!!");
+				attroff(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
 			}
-				break;
+			break;
 
-		case KEY_F(1):
+		case SAVE:
 			wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
 
-			mvaddstr(num_rows - 2, num_cols - 45, "Would you like to save your file? Y or N");
+			mvaddstr(term_rows - 2, term_cols - 45, "Would you like to save your file? Y or N");
 
 			wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
-			refresh();
+			wrefresh(main_window);
 			if (confirm()) {
 				wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
-				mvaddstr(((num_rows / 2) - 2), ((num_cols / 2) - 12), "Your file has been saved!!");
+				mvaddstr(((term_rows / 2) - 2), ((term_cols / 2) - 12), "Your file has been saved!!");
 				wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
 			}
 			break;
 
-		case KEY_F(2):
-			wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
+		case LOAD:
+			attron(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
 
-			mvaddstr(num_rows - 2, num_cols - 45, "Would you like to load a file? Y or N");
+			mvaddstr(term_rows - 2, term_cols - 45, "Would you like to load a file? Y or N");
 
-			wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
-			refresh();
+			attroff(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
+			//wrefresh(main_window);
 			break;
 
-		case KEY_F(3):
-			wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
+		case NEW_FILE:
+			attron(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
 
-			mvaddstr(num_rows - 2, num_cols - 45, "Would you like to make a new file?");
+			mvaddstr(term_rows, term_cols - 45, "Would you like to make a new file? Y or N");
 
-			wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
-			refresh();
+			attroff(COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
+			//refresh(main_window);
 			break;
 
 		case KEY_RESIZE:
 
-				//getmaxyx(stdscr, num_rows, num_cols);
-				resize_term(0, 0);
-				getmaxyx(stdscr, num_rows, num_cols);
-				wresize(main_window,num_rows, num_cols);
-				//resize_term(0, 0);
-				//Make border
-				wborder(main_window, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK);
-				//Header message
-				wattron(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
-				mvaddstr(0, 3, "Welcome to the Airth Text Editor!");
-				wattroff(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS
-				//Footer message
-				wattron(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
-				mvaddstr(num_rows - 1, 3, "ESC = Quit Program | CTRL+S = Save | CTRL+L = Load");
-				wattroff(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS				
-				//wmove(main_window, 3, 3);
-				//refresh();
-				wrefresh(main_window);
-				//wmove(main_window, 3, 3);
-				input = NULL;
-				break;
+			//getmaxyx(stdscr, num_rows, num_cols);
+			//resize_term(0, 0);				
+			//wresize(main_window,num_rows, num_cols);
+			//resize_term(0, 0);
+			//Make border
+			//wborder(main_window, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK);
+			//Header message
+			//wattron(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
+			//mvaddstr(0, 3, "Welcome to the Airth Text Editor!");
+			//wattroff(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS
+			//Footer message
+			//wattron(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
+			//mvaddstr(num_rows - 1, 3, "ESC = Quit Program | CTRL+S = Save | CTRL+L = Load");
+			//wattroff(main_window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS				
+			//wmove(main_window, 3, 3);
+			//refresh();
+			//wrefresh(main_window);
+			//wmove(main_window, 3, 3);
+			input = NULL;
+			break;
 
-		case KEY_COMMAND:
-			wattron(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR FOR PROMPT
-			mvaddstr(((num_rows / 2) - 2), ((num_cols / 2) - 12), "Your thing has f8!!");
-			wattroff(main_window, COLOR_PAIR(PROMPTCOLORS)); //CHANGES COLOR BACK TO WINCOLORS
-			
-			break;	
+
+		case KEY_LEFT:
+
+			getyx(main_window, curs_y, curs_x);
+			move(curs_y, curs_x - 1);
+			//refresh();
+			getch();
+			break;
+
+
 
 		default:
+
 			waddch(main_window, input);
-			input = getch();
+			input = wgetch(main_window);
 			break;
-			}
-
-
-
-
-
-
 		}
 
-		char response = getch();
-		endwin();
+
+
+
+
 
 	}
 
+	char response = getch();
+	endwin();
+
+}
 
 
-	bool confirm(void) {
-		char answer = getch();
-		switch (answer) {
 
-		case 'y':
-			return true;
-			break;
-		case 'Y':
-			return true;
-			break;
+bool confirm(void) {
+	char answer = getch();
+	switch (answer) {
 
-		case 'n':
-			return false;
-			break;
+	case 'y':
+		return true;
+		break;
+	case 'Y':
+		return true;
+		break;
 
-		case 'N':
-			return false;
-			break;
+	case 'n':
+		return false;
+		break;
 
-		default:
-			confirm();
-		}
+	case 'N':
+		return false;
+		break;
 
+	default:
+		confirm();
 	}
 
-	void resizeWin(WINDOW* window, int* rows, int* cols) {
-		//resize_term(0, 0);
-		getmaxyx(window, *rows, *cols);
-		resize_term(*rows - 1, *cols - 1);
-		getmaxyx(window, *rows, *cols);
+}
 
+void windowResize(WINDOW* win, int rows, int cols) {
 
-		//creates the border around the perimeter of the window
-		for (int i = 0; i < *cols; i++)
-		{
-			//top row
-			mvaddch(0, i, ACS_DIAMOND);
+	resize_window(win, (rows - 4), (cols - 4));
 
-
-			//bottom row
-			mvaddch(*rows - 1, i, ACS_BLOCK);
-
-		}
-
-		for (int i = 0; i < *rows; i++)
-		{
-			//left column
-			mvaddch(i, 0, ACS_BLOCK);
-
-			//right column
-			mvaddch(i, *cols - 1, ACS_BLOCK);
-		}
-
-
-
-		//add Welcome String to the screen
-		wattron(window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
-
-		mvaddstr(0, 3, "Welcome to the Airth Text Editor!");
-
-		wattroff(window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS
-
-
-
-		//Footer message
-		wattron(window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR FOR PROMPT
-
-		mvaddstr(*rows - 1, 3, "ESC = Quit Program | CTRL+S = Save | CTRL+L = Load");
-
-		wattroff(window, COLOR_PAIR(WINDOWTEXT)); //CHANGES COLOR BACK TO WINCOLORS
-
-	}
-	
+}
